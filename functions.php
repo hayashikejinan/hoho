885126bae2c603ad42b24c78ad88b3337d6da452
hayashikejinan <hayashikejinan@gmail.com>
2013-09-07 15:16:33 +0900

    functions: functions.php 作成

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * functions
+ *
+ * テーマでは、functions.phpという名前の関数ファイルを使用することができます
+ * 基本的にプラグインのように動作し、テーマに存在していれば自動的にWordPressの初期化時に読み込まれます
+ *
+ * @package    WordPress
+ * @subpackage Hoho
+ */
