a706881e4a1a20fa16c699dcf9abe7a99ee37ebf
hayashikejinan <hayashikejinan@gmail.com>
2013-09-08 22:58:54 +0900

    footer: <?php wp_footer(); ?> を記述
    Google Analitycs スクリプトはプラグインで設定するケースが多いため消す

--- footer.php
+++ footer.php
@@ -24,16 +24,6 @@
 <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/plugins.js"></script>
 <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/main.js"></script>
 
-<script>
-	var _gaq = [
-		['_setAccount', 'UA-XXXXX-X'],
-		['_trackPageview']
-	];
-	(function (d, t) {
-		var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
-		g.src = '//www.google-analytics.com/ga.js';
-		s.parentNode.insertBefore(g, s)
-	}(document, 'script'));
-</script>
+<?php wp_footer(); ?>
 </body>
 </html>
