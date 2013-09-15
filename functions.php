e4a79bdaa15aea4b88fd92fe36ba31fafc23973f
hayashikejinan <hayashikejinan@gmail.com>
2013-09-15 20:35:35 +0900

    functions: サイドバーウィジェットを使えるように定義する

--- functions.php
+++ functions.php
@@ -15,3 +15,27 @@
  * @since WordPress 3.0
  */
 add_theme_support( 'automatic-feed-links' );
+
+/**
+ * メインのサイドバーを定義
+ *
+ * @since WordPress 2.2 (2.9.0: description プロパティ追加
+ */
+register_sidebar( $args = array(
+		// サイドバーの名前、2つめの引数でテキストドメインを指定
+		'name'          => __( 'メインのサイドバー', 'テーマのテキストドメイン' ),
+		// サイドバー呼び出し用のID。小文字かつスペースは無きよう。
+		'id'            => 'sidebar-main',
+		// サイドバーの説明
+		'description'   => '',
+		// サイドバーウィジェットに付加されるクラス
+		'class'         => '',
+		// ウィジェットの前に配置する HTML
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		// ウィジェットの後に配置する HTML
+		'after_widget'  => '</aside>',
+		// ウィジェットタイトルの前に配置する HTML
+		'before_title'  => '<h3 class="widgettitle">',
+		// ウィジェットタイトルの後に配置する HTML
+		'after_title'   => '</h3>' )
+);
