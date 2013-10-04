8ebbe39d88b073eefadd9d93bd1678cd2aae05eb
hayashikejinan <hayashikejinan@gmail.com>
2013-10-04 22:01:58 +0900

    index: ページネーションを実装

--- index.php
+++ index.php
@@ -21,6 +21,13 @@ get_header(); ?>
 		// content.php を読み込む
 		get_template_part( 'content' ); ?>
 
+		<!-- pagination -->
+		<div class="navigation">
+			<div class="alignleft"><?php previous_posts_link( '&laquo; 前へ' ) ?></div>
+			<div class="alignright"><?php next_posts_link( '次へ &raquo;', '' ) ?></div>
+		</div>
+		<!-- pagination -->
+
 	</div>
 	<!-- #main -->
 
