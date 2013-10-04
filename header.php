4010c1e5ae1df4fd152f758ff1868cfe4f074928
hayashikejinan <hayashikejinan@gmail.com>
2013-10-04 21:09:52 +0900

    header: コメント関数用のスクリプト

--- header.php
+++ header.php
@@ -31,6 +31,7 @@
 
 	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
 
+	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
