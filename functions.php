c7f65bd57effae7c6e336383e4e179f9dcf9a9ef
hayashikejinan <hayashikejinan@gmail.com>
2013-10-08 16:09:11 +0900

    functions: テーマ用の wp_enqueue アクションフック（サンプルをコピペ

--- functions.php
+++ functions.php
@@ -86,6 +86,17 @@ add_action( 'after_setup_theme', 'hoho_setup' );
 
 
 /**
+ * スクリプトとスタイルのエンキュー、アクションフック
+ */
+function theme_name_scripts() {
+	wp_enqueue_style( 'style-name', get_stylesheet_uri() );
+	wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
+}
+
+add_action( 'wp_enqueue_scripts', 'theme_name_scripts' );
+
+
+/**
  * メインのサイドバーを定義
  *
  * @since WordPress 2.2 (2.9.0: description プロパティ追加
