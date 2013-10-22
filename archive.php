30af7a4906020e97ebff00bc16ab9ebe5fed4106
hayashikejinan <hayashikejinan@gmail.com>
2013-10-22 09:23:11 +0900

    ページネーションを独立、css修正

--- archive.php
+++ archive.php
@@ -28,12 +28,7 @@ get_header(); ?>
 		// content.php を読み込む
 		get_template_part( 'content' ); ?>
 
-		<!-- pagination -->
-		<div class="navigation">
-			<div class="alignleft"><?php previous_posts_link( '&laquo; 前へ' ) ?></div>
-			<div class="alignright"><?php next_posts_link( '次へ &raquo;', '' ) ?></div>
-		</div>
-		<!-- pagination -->
+		<?php get_template_part( 'inc/pagination' ); ?>
 
 	</div>
 	<!-- #main -->
---
+++ inc/pagination.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * ページネーション
+ */
+?>
+
+<nav class="navigation">
+	<div class="alignleft"><?php previous_posts_link( __( '&laquo; Previous' ) ) ?></div>
+	<div class="alignright"><?php next_posts_link( __( 'Next &raquo;' ), '' ) ?></div>
+</nav>
