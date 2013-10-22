f674a10e812f37047b4cf44e17f5e7dfcbb7212f
hayashikejinan <hayashikejinan@gmail.com>
2013-10-22 23:09:50 +0900

    functions: wp_enqueue_script の modernizr バージョンを修正

--- functions.php
+++ functions.php
@@ -111,7 +111,7 @@ function hoho_scripts() {
 		wp_enqueue_script( 'comment-reply' );
 	// Modernizr ライブラリ
 	wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js',
-		array(), '2.6.2', false );
+		array(), '2.6.2-1.1.0', false );
 	// レスポンシブメニュー
 	wp_enqueue_script( 'sidr', get_template_directory_uri() . '/js/jquery.sidr.min.js', array(), '1.1.1', true );
 	// メインの js
